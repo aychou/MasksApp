ba277f7fd14ecd93e5931653fc62a1581c87a10c
aychou <32346173+aychou@users.noreply.github.com>
2020-07-29 14:38:59 -0700

    should work

--- app/src/main/java/com/example/masks/Flight.java
+++ app/src/main/java/com/example/masks/Flight.java
@@ -8,38 +8,69 @@ import static com.example.masks.GameView.screenRatioX;
 import static com.example.masks.GameView.screenRatioY;
 
 public class Flight {
+    int toShoot = 0;
+    boolean isGoingUp = false;
+    int x,y,width,height, wingCounter = 0, shootCounter = 1;
+    Bitmap flight1,flight2,shoot1,shoot2,shoot3,shoot4,shoot5;
+    private GameView gameView;
 
-    public boolean isGoingUp=false;
-    int x, y, width, height, wingCounter=0;
-    Bitmap flight1, flight2;
+    Flight(GameView gameView,int screenY, Resources res) {
 
-    Flight(int screenY, Resources res) {
+        this.gameView = gameView;
 
-        flight1= BitmapFactory.decodeResource(res, R.drawable.fly1);
-        flight2= BitmapFactory.decodeResource(res, R.drawable.fly2);
-
+        flight1=BitmapFactory.decodeResource(res, R.drawable.fly1);
+        flight1=BitmapFactory.decodeResource(res, R.drawable.fly1); //should be fly2
         width = flight1.getWidth();
         height = flight1.getHeight();
-
-        width/=4;
-        height/=4;
-
-        width *=(int) screenRatioX;
+        width /= 4;
+        height /= 4;
+        width *= (int) screenRatioX;
         height *= (int) screenRatioY;
 
-        flight1 = Bitmap.createScaledBitmap(flight1, width, height, false);
-        flight2 = Bitmap.createScaledBitmap(flight2, width, height, false);
+        flight1 = Bitmap.createScaledBitmap(flight1,width,height, false);
+        flight2 = Bitmap.createScaledBitmap(flight2,width,height, false);
 
-        y = screenY/2;
-        x= (int)(64 * screenRatioX);
+        shoot1 = BitmapFactory.decodeResource(res, R.drawable.shoot1);
+        shoot2 = BitmapFactory.decodeResource(res, R.drawable.shoot2);
+        shoot3 = BitmapFactory.decodeResource(res, R.drawable.shoot3);
+        shoot4 = BitmapFactory.decodeResource(res, R.drawable.shoot4);
+        shoot5 = BitmapFactory.decodeResource(res, R.drawable.shoot5);
+
+        shoot1 = Bitmap.createScaledBitmap(shoot1, width, height, false);
+        shoot2 = Bitmap.createScaledBitmap(shoot2, width, height, false);
+        shoot3 = Bitmap.createScaledBitmap(shoot3, width, height, false);
+        shoot4 = Bitmap.createScaledBitmap(shoot4, width, height, false);
+        shoot5 = Bitmap.createScaledBitmap(shoot5, width, height, false);
+
+        y = screenY / 2;
+        x = (int)(64 * screenRatioX);
     }
+
     Bitmap getFlight () {
-        if(wingCounter==0) {
+        if(toShoot != 0) {
+            if(shootCounter == 1) {
+                return shoot1;
+            }
+            if(shootCounter == 2) {
+                return shoot2;
+            }
+            if(shootCounter == 3) {
+                return shoot3;
+            }
+            if(shootCounter == 4) {
+                return shoot4;
+            }
+            shootCounter = 1;
+            toShoot--;
+            gameView.newBullet();
+            return shoot5;
+        }
+        if(wingCounter == 0){
             wingCounter++;
             return flight1;
         }
         wingCounter--;
-
         return flight2;
     }
 }
+
--- app/src/main/java/com/example/masks/GameView.java
+++ app/src/main/java/com/example/masks/GameView.java
@@ -6,18 +6,21 @@ import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameView extends SurfaceView implements Runnable {
     private Thread thread;
     private boolean isPlaying;
     private int screenX, screenY;
     public static float screenRatioX, screenRatioY;
     private Paint paint;
+    private List<Bullet> bullets;
     private Flight flight;
     private Background background1, background2;
 
     public GameView(Context context, int screenX, int screenY) {
         super(context);
-
         this.screenX = screenX;
         this.screenY = screenY;
         screenRatioX = 1920f/screenX;
@@ -25,9 +28,11 @@ public class GameView extends SurfaceView implements Runnable {
         background1 = new Background(screenX, screenY, getResources());
         background2 = new Background(screenX, screenY, getResources());
 
-        background2.x = screenX;
+        flight = new Flight(this, screenY, getResources());
 
-        flight= new Flight(screenY, getResources());
+        bullets = new ArrayList<>();
+
+        background2.x = screenX;
 
         paint = new Paint();
     }
@@ -51,17 +56,27 @@ public class GameView extends SurfaceView implements Runnable {
         if(background2.x+background2.background.getWidth()<0){
             background2.x = screenX;
         }
-        if (flight.isGoingUp)
-            flight.y-=30 * screenRatioY;
+        if(flight.isGoingUp)
+            flight.y -= 30 * screenRatioY;
         else
-            flight.y+=30 *screenRatioY;
+            flight.y += 30 * screenRatioY;
+        if(flight.y < 0)
+            flight.y = 0;
+        if(flight.y >= screenY - flight.height)
+            flight.y = screenY - flight.height;
 
-        if(flight.y<0){
-            flight.y=0;
+        List<Bullet> trash = new ArrayList<>();
+
+        for(Bullet bullet : bullets) {
+            if(bullet.x > screenX)
+                trash.add(bullet);
+
+            bullet.x += 50 * screenRatioX;
         }
-        if(flight.y>screenY-flight.height){
-            flight.y=screenY-flight.height;
-        }
+
+        for(Bullet bullet : trash)
+            bullets.remove(bullet);
+
     }
     private void draw () {
 
@@ -72,10 +87,11 @@ public class GameView extends SurfaceView implements Runnable {
 
             canvas.drawBitmap(flight.getFlight(), flight.x, flight.y, paint);
 
+            for (Bullet bullet : bullets)
+                canvas.drawBitmap(bullet.bullet, bullet.x, bullet.y, paint);
+
             getHolder().unlockCanvasAndPost(canvas);
         }
-
-
     }
     private void sleep () {
         try {
@@ -101,17 +117,26 @@ public class GameView extends SurfaceView implements Runnable {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
-        switch (event.getAction()){
+        switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if (event.getX()<screenX/2){
+                if(event.getX() < screenX / 2){
                     flight.isGoingUp = true;
                 }
                 break;
             case MotionEvent.ACTION_UP:
-                flight.isGoingUp=false;
+                flight.isGoingUp = false;
+                if(event.getX() > screenX / 2)
+                    flight.toShoot++;
                 break;
         }
 
         return true;
+    }
+
+    public void newBullet() {
+        Bullet bullet = new Bullet(getResources());
+        bullet.x = flight.x + flight.width;
+        bullet.y = flight.y + (flight.height / 2);
+        bullets.add(bullet);
     }
 }
